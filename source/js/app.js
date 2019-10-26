3f9d91e348ec98bc28a434c8062983efeeb4a72c
SaigyoujiYuyuko <HGK-SaigyoujiYuyuko@outlook.com>
2019-10-26 09:21:53 +0800

    :hammer: :bug: 去除侧边栏跟随

--- source/js/app.js
+++ source/js/app.js
@@ -30,37 +30,3 @@ for (let i = 0; i < covers.length; i++){
   covers[i].src = "https://uuz-blog.oss-cn-hangzhou.aliyuncs.com/images/post-covers/" + Math.round(Math.random() * (7-1)) + ".jpg";
 }
 
-/*
-侧边栏跟随
- */
-
-let drawer = $(".nexmoe-drawer")[0];
-
-let scroll_hight_provous = 0;
-
-document.onscroll = function (){
-  let scroll_hight_now = document.body.scrollTop + document.documentElement.scrollTop;
-
-  //console.log("pvs: " + scroll_hight_provous + " ||| now: " + scroll_hight_now)
-
-  // 自动往下
-  if (scroll_hight_now > scroll_hight_provous && scroll_hight_now > $(".nexmoe-primary")[0].scrollHeight*0.2){
-    drawer.style.position = "fixed";
-    $(drawer).css("top", "auto");
-
-    scroll_hight_provous = scroll_hight_now;
-  }
-  
-  if (scroll_hight_now < scroll_hight_provous) {
-    drawer.style.position = "absolute";
-    $(drawer).css("top", scroll_hight_now);
-    //$(drawer).css("top", document.body.scrollTop);
-    //$(drawer).css("top", document.body.scrollTop + document.documentElement.scrollTop);
-
-    scroll_hight_provous = scroll_hight_now;
-  }
-
-
-
-};
-
